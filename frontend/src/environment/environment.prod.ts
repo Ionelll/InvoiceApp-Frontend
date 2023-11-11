1b1d0975de92d1e246d933e9f3d2b791e26bb587
Ionelll <serban.nelutu@gmail.com>
2023-11-11 08:17:24 +0100

    removed sensitive files

--- frontend/src/environment/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://invoicefree-test.onrender.com:443/api',
-};
--- frontend/src/environment/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
-  locationUrl:
-    'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=',
-  placesKey: 'AIzaSyBA1zSsfrFni-rUTWZz3okBiUaaZGTIB0Y',
-};
